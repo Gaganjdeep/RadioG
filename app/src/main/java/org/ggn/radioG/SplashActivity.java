6bc5468e3180c8f5127ac09ea04f660d8cc8ca4a
gagandeep <gagandeep.singh@amebasoftwares.com>
2017-02-08 13:26:08 +0530

    static url added  for radio

--- app/src/main/java/org/ggn/radioG/SplashActivity.java
+++ app/src/main/java/org/ggn/radioG/SplashActivity.java
@@ -2,22 +2,14 @@ package org.ggn.radioG;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 
 import org.ggn.radioG.core.Station;
-import org.ggn.radioG.helpers.ConstantKeys;
 import org.ggn.radioG.helpers.StorageHelper;
-import org.ggn.radioG.webhelperG.WebServicehelperG;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.io.File;
-import java.io.IOException;
-
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.Response;
 
 public class SplashActivity extends Activity
 {
@@ -27,19 +19,13 @@ public class SplashActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-//        System.setProperty("http.keepAlive", "false");
 
-        WebServicehelperG.getInstance().GET(ConstantKeys.GET_STATIONS_LIST, new Callback()
+
+        new AsyncTask<Void, Void, Void>()
         {
-            @Override
-            public void onFailure(Call call, IOException e)
-            {
-
-                startActivity(new Intent(SplashActivity.this, MainActivity.class));
-            }
 
             @Override
-            public void onResponse(Call call, Response response) throws IOException
+            protected Void doInBackground(Void... voids)
             {
                 try
                 {
@@ -47,31 +33,75 @@ public class SplashActivity extends Activity
                     File          mFolder       = storageHelper.getCollectionDirectory();
 
 
-                    JSONObject jsonObject = new JSONObject(response.body().string());
-
-                    if (jsonObject.getString("Status").equals("success"))
-                    {
-                        JSONArray jsonArray = jsonObject.getJSONArray("Message");
-
-                        for (int i = 0; i < jsonArray.length(); i++)
-                        {
-                            JSONObject jobjInner = jsonArray.getJSONObject(i);
-
-                            new Station(mFolder, jobjInner.getString("FilePath"));
-                        }
-                    }
-
+                    new Station(mFolder, "http://69.175.94.98:8146");
                 }
                 catch (Exception e)
                 {
                     e.printStackTrace();
                 }
+                return null;
+            }
 
-
-                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            @Override
+            protected void onPostExecute(Void aVoid)
+            {
+                super.onPostExecute(aVoid);
+                new Handler().postDelayed(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                    }
+                }, 4000);
 
             }
-        });
+        }.execute();
+
+
+//        WebServicehelperG.getInstance().GET(ConstantKeys.GET_STATIONS_LIST, new Callback()
+//        {
+//            @Override
+//            public void onFailure(Call call, IOException e)
+//            {
+//
+//                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+//            }
+//
+//            @Override
+//            public void onResponse(Call call, Response response) throws IOException
+//            {
+//                try
+//                {
+//                    StorageHelper storageHelper = new StorageHelper(SplashActivity.this);
+//                    File          mFolder       = storageHelper.getCollectionDirectory();
+//
+//
+//                    JSONObject jsonObject = new JSONObject(response.body().string());
+//
+//                    if (jsonObject.getString("Status").equals("success"))
+//                    {
+//                        JSONArray jsonArray = jsonObject.getJSONArray("Message");
+//
+//                        for (int i = 0; i < jsonArray.length(); i++)
+//                        {
+//                            JSONObject jobjInner = jsonArray.getJSONObject(i);
+//
+//                            new Station(mFolder, jobjInner.getString("FilePath"));
+//                        }
+//                    }
+//
+//                }
+//                catch (Exception e)
+//                {
+//                    e.printStackTrace();
+//                }
+//
+//
+//                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+//
+//            }
+//        });
 
 
     }
